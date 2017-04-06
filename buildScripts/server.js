64d39470022287836f752b55e0205e138fbad0db
Nana <nana.o.baah@gmail.com>
2017-04-07 01:33:05 +0200

    devtools update and rigid linting rules

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -8,20 +8,22 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+// eslint-disable-next-line
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
-}))
+}));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+    // eslint-disable-next-line
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
     if (err) {
+        // eslint-disable-next-line
         console.log(err);
-    }
-    else {
+    } else {
         open(`http://localhost:${port}`);
     }
 });
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './index.css';
 import numeral from 'numeral';
 
 const val = numeral(1000).format('$0,0.00');
+
+// eslint-disable-next-line
 console.log(`I would pay you ${val}`);
